c712ff60daebb4b8adae3c888b71ce28d43922a1
ajax6613 <70900281+ajax6613@users.noreply.github.com>
2020-09-07 16:25:17 -0300

    Add files via upload

---
+++ diccionario_alumnos.py
@@ -0,0 +1,6 @@
+Pedro={'programacion':'6', 'fisica'='6'}
+Juan={'programacion':'5', 'fisica':'5'}
+Diego={'programacion':'4', 'fisica':'4'}
+Cristobal={'programacion':'3', 'fisica':'3'}
+Cristobal=['programacion']
+Cristobal=['fisica']
